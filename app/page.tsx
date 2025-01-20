6dd0c60ea2c7a01a82cb701dfa7592e9aeebe447
Chandu Ranwala <chanduranwala@Mac.scanbot-bonn>
2025-01-20 15:58:36 +0100

    updated to the latest SDK

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
     async function init() {
       await ScanbotSDK.initialize({
         licenseKey: "",
-        engine: "/wasm/"
+        enginePath: "/wasm/"
       });
     }
     init();
